cdb932cc432ff9cad3c6225abb7556c01d4e4c49
matmalone <matmalone@846ccc3c-5622-0410-843d-adcfae285e5a>
2006-11-24 20:15:21 +0000

    some skeleton code and license header

--- trunk/slideshowpresenter.py
+++ trunk/slideshowpresenter.py
@@ -1,3 +1,36 @@
 #!/usr/bin/python
 
+# fsshow -- A simple slideshow generator for Flickr hosted images.
+# Copyright (C) 2006 Mat Malone
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+class SlideshowPresenter(object):
+    """
+    Standard presenter class for the fsshow program.
+    """
+    def __init__(self, model, view, interactor):
+        self.model = model
+        self.view = view
+        self.isListening = True
+        #interactor.Install(self, view)
+        self.isListening = True
+        #self.initView()
+        #view.start()
+        
+if __name__ == "__main__":
+    import slideshowmodel
+    presenter = SlideshowPresenter(slideshowmodel.SlideshowModel(), None, None)
+    
