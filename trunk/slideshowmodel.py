fc064df88999f04535be7e28f793ee12ba2d8f31
matmalone <matmalone@846ccc3c-5622-0410-843d-adcfae285e5a>
2006-11-25 02:28:00 +0000

    rearranged imports

--- trunk/slideshowmodel.py
+++ trunk/slideshowmodel.py
@@ -17,7 +17,12 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-import flickr, util, urllib, os, time, threading
+import flickr
+import util
+import urllib
+import os
+import time
+import threading
 
 class SlideshowModel(object):
     """
