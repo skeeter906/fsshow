a9bb61c8367483e60d815e2348ecc6f636f92d5e
matmalone <matmalone@846ccc3c-5622-0410-843d-adcfae285e5a>
2006-11-27 22:36:13 +0000

    work-around for py2exe

--- imaging.py
+++ imaging.py
@@ -17,8 +17,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from PIL import Image
 import shutil
+from PIL import Image
+from PIL import JpegImagePlugin
+Image._initialized=2
+
 
 class FitImage(object):
     def __init__(self, imagePath):
